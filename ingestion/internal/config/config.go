8cbcfb8f99dd34b64f3b5155a6e1f5e45b039a1b
KryukovEgor <egozay1337@gmail.com>
2026-09-19 01:53:55 +0300

    chore(config): add aggregator config struct

--- ingestion/internal/config/config.go
+++ ingestion/internal/config/config.go
@@ -1,14 +1,29 @@
 package config
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 type Config struct {
-	Logger LoggerConfig `yaml:"logger"`
+	Logger     LoggerConfig     `yaml:"logger"`
+	Aggregator AggregatorConfig `yaml:"aggregator"`
 }
 
 type LoggerConfig struct {
 	Level  string `yaml:"level"`
 	Format string `yaml:"format"`
+}
+
+type AggregatorConfig struct {
+	DigitalSpb DigitalSpbConfig `yaml:"digitalspb"`
+}
+
+type DigitalSpbConfig struct {
+	MaxIdleConns        int           `yaml:"max_idle_conns"`
+	MaxIdleConnsPerHost int           `yaml:"max_idle_conns_per_host"`
+	MaxConnsPerHost     int           `yaml:"max_conns_per_host"`
+	RequestTimeout      time.Duration `yaml:"request_timeout"`
 }
 
 var config *Config
