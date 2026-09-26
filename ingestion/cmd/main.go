fe9a42d6e25438bf5ece4e5a5fc676ecac0b7ad5
KryukovEgor <egozay1337@gmail.com>
2026-09-26 21:08:34 +0300

    feat(db): add migrations logic, update Dockerfile structure

--- ingestion/cmd/main.go
+++ ingestion/cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/kotafan1rich/GeoLogic-Monitor/ingestion/internal/app"
@@ -15,6 +16,11 @@ func main() {
 	config.MustLoad(configPath)
 
 	a := app.New()
+
+	err := a.Migrate(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	if err := a.Run(context.Background()); err != nil {
 		fmt.Fprintf(os.Stderr, "app run failed: %v\n", err)
--- ingestion/internal/app/app.go
+++ ingestion/internal/app/app.go
@@ -4,14 +4,21 @@ import (
 	"context"
 	"fmt"
 	"log/slog"
+	"os"
 	"os/signal"
 	"syscall"
 	"time"
 
+	"github.com/jackc/tern/v2/migrate"
 	"github.com/kotafan1rich/GeoLogic-Monitor/ingestion/internal/closer"
 )
 
-const closeDuration = 10 * time.Second
+const (
+	closeDuration = 10 * time.Second
+
+	ternTable     = "tern_version"
+	migrationsDir = "./migrations"
+)
 
 type App struct {
 	di *diContainer
@@ -22,7 +29,7 @@ func New() *App {
 }
 
 func (a *App) Run(ctx context.Context) error {
-	const op = "ingestion.app.App.Run"
+	const op = "app.App.Run"
 
 	log := a.di.Logger()
 
@@ -62,6 +69,43 @@ func (a *App) Run(ctx context.Context) error {
 	}
 
 	log.InfoContext(ctx, "application stopped")
+
+	return nil
+}
+
+func (a *App) Migrate(ctx context.Context) error {
+	const op = "app.App.Migrate"
+
+	log := a.di.Logger()
+
+	pool := a.di.DB(ctx).Pool()
+
+	conn, err := pool.Acquire(ctx)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+	defer conn.Release()
+
+	migrator, err := migrate.NewMigrator(ctx, conn.Conn(), ternTable)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	log.InfoContext(ctx, "migrator initialized")
+
+	err = migrator.LoadMigrations(os.DirFS(migrationsDir))
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	log.InfoContext(ctx, "migrations loaded")
+
+	err = migrator.Migrate(ctx)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	log.InfoContext(ctx, "migration finished")
 
 	return nil
 }
